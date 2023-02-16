a26e80f6f570a8329b7cbc948529b2a928a3b6c7
yilmar Munioz <yilmar.munioz@hotmail.com>
2023-02-16 00:17:06 -0300

    Proyecto v1

--- js/main_update.js
+++ js/main_update.js
@@ -1,66 +1,95 @@
-import datajson from "../data.json" assert { type: "json" };
-import Tramo from "./tramo.js";
+import dataset from "../data.json" assert { type: "json" };
+import Tramo from "./Entities/tramo.js";
 
 //Inicializar Variables
+let listHistoric = [];
+let containerCard = document.getElementById("containerCard");
+var stringContainerCard = "";
 
 //Inicializar html
 let selectOrigen = document.getElementById("SelectOrigen");
 let selectDestino = document.getElementById("SelectDestino");
 
-let containerPrincipal = document.getElementById("containerPrincipal");
-let btnInsertPackage = document.getElementById("btnInsertPackage");
+//Revisamos si tenemos data en LocalStorage
+listHistoric = JSON.parse(localStorage.getItem("listHistoric"));
+listHistoric.forEach((element) => {
+  stringContainerCard +=
+    '<div class="col d-flex align-items-start"><div><h4>' +
+    element.Origen.concat("-", element.Destino) +
+    "</h4><p>" +
+    element.Valor +
+    "</p></div></div>";
+});
+containerCard.innerHTML = stringContainerCard;
 
-
-//Cargar listas
-datajson.forEach((element) => {
+  
+//Cargar listas por json local
+dataset.forEach((element) => {
   let option = document.createElement("option");
   let { name } = element;
   option.value, (option.text = name);
   selectOrigen.appendChild(option);
 });
-datajson.forEach((element) => {
-  let option = document.createElement("option");
-  let { name } = element;
-  option.value, (option.text = name);
-  selectDestino.appendChild(option);
-});
+
+//!Cargar lista por una API utilizando fecth (No encontre api online de ciudades Chilena) por eso no  puede usar await
+fetch("/data.json")
+  .then((response) => response.json())
+  .then((data) => {
+    data.forEach((element) => {
+      let option = document.createElement("option");
+      let { name } = element;
+      option.value, (option.text = name);
+      selectDestino.appendChild(option);
+    });
+  });
+
 
 //Boton insertar paquetes
-btnInsertPackage.onclick = () => {
-
+btnCalcularViaje.onclick = () => {
   const tramo = new Tramo(selectOrigen.value, selectDestino.value);
 
   if (tramo.validateEqualTramo()) {
     alert("Son iguales");
   } else {
-    if (tramo.isExistTramo(selectOrigen.value) ||tramo.isExistTramo(selectDestino.value)) {
+    if (
+      tramo.isExistTramo(selectOrigen.value) ||
+      tramo.isExistTramo(selectDestino.value)
+    ) {
       alert("No existe el tramo, favor revisar opcion 2");
     } else {
       //Ahora calcular el valor por las dimesiones del paquete
       let valor = tramo.getValueTramo(selectOrigen.value, selectDestino.value);
 
-      containerPrincipal.style.display = "block";
-      let countPackage = document.getElementById("txtCountPackage"); // Aun no se implemeta el for por cada paquete ingresado.
-      let containerPaquete = document.getElementById("containerPaquete");
-
-      //Inserta acá el for para cada Card pequeña
-      containerPaquete.innerHTML =
-        '<div class="col-auto"> <input type="number" class="form-control" id="txtLargo" placeholder="Largo"></div><br><div class="col-auto"> <input type="number" class="form-control" id="txtAncho" placeholder="Ancho"></div><br><div class="col-auto"><input type="number" class="form-control" id="txtAlto" placeholder="Alto"></div><br>';
-
-      //Creamos el boton
-      let btn = document.createElement("button");
-      btn.innerHTML = "Calcular";
-      btn.className = "w-100 btn btn-lg btn-outline-primary";
-      btn.addEventListener("click", function () {
-        //Buscar la forma de recuperar datos dinamicos
-        let txtLargo = parseInt(document.getElementById("txtLargo").value);
-        let txtAncho = parseInt(document.getElementById("txtLargo").value);
-        let txtAlto = parseInt(document.getElementById("txtLargo").value);
-        let CalculoPaquete = parseInt(txtAncho * txtAlto * txtLargo);
-        let MontoTotal = valor + CalculoPaquete;
-        alert(MontoTotal);
+      //Guardamos el historial
+      let viaje = {
+        "Origen":selectOrigen.value,
+        "Destino":selectDestino.value,
+        "Valor":valor
+      }
+      listHistoric.push(viaje);
+      console.log(listHistoric);
+      localStorage.setItem("listHistoric",JSON.stringify(listHistoric));
+      Swal.fire({
+        title: "¿Quieres Guardar el Viaje?",
+        showDenyButton: true,
+        showCancelButton: true,
+        confirmButtonText: "Si",
+        denyButtonText: `No`,
+      }).then((result) => {
+        /* Read more about isConfirmed, isDenied below */
+        if (result.isConfirmed) {
+          Swal.fire("Guardado!");
+          setTimeout(function(){
+            window.location.reload();
+        }, 1500);
+          
+        } else if (result.isDenied) {
+          Swal.fire("No se Guardo El viaje");
+          setTimeout(function(){
+            window.location.reload();
+        }, 1500);
+        }
       });
-      containerPaquete.appendChild(btn);
     }
   }
 };
